799647f0109fe2277bb1f6cf367442d1f39bc648
Batuntu <67765337+Batuntu@users.noreply.github.com>
2020-07-10 15:43:03 -0400

    Comentarios
    -Se añadieron comentarios a las funciones refiriéndose a su funcionamiento

--- Funciones.h
+++ Funciones.h
@@ -9,27 +9,155 @@ typedef struct tipoAuto tipoAuto;
 typedef struct tipoUsuario tipoUsuario;
 typedef struct tipoMejora tipoMejora;
 
-extern int menu1(void);
-int portada(void);
-int menuAutos(void);
+/**
+Lee los datos del usuario y busca su existencia en el mapa usuarios.
+
+Retorna un Bool para verificar ingreso exitoso de usuario.
+
+*/
 bool ingresarUsuario();
+
+/**
+Funcion Hash encargada de otorgar una posicion en los mapas.
+
+Recibe una key que se usara para obtener la posicion.
+*/
 long long stringHash(const void *);
+
+
+/**
+Funcion encargada de comparar datos.
+
+Recibe dos datos cualquiera que se deseen comparar.
+
+Esta funcion devuelve un 0 en caso de ser diferentes y 1 en caso contrario.
+*/
 int stringEqual(const void *, const void *);
+
+
+/**
+Lee los archivos para obtener los datos necesarios y los almacena en los TDA.
+
+Recibe como parametros los diferentes mapas a llenar.
+*/
 void llenarBD(Map *, Mapx *, Mapx *);
+
+/**
+
+
+*/
 char * _strdup (const char *);
+
+
+/**
+Obtiene caracteres de una linea de texto hasta llegar a un ";".
+
+Los parametros que recibe son: la linea de texto y la posicion que se desea obtener;
+
+Entrega un puntero a los caracteres.
+*/
 const char *get_csv_field (char *, int );
+
+/**
+Busca registrar a los nuevos usuarios y ubicarlos dentro del sistema.
+
+Recibe el Mapa donde se ubican todos los usuarios.
+*/
 void registrarUsuario(Map *);
+
+/**
+
+*/
 void *crearChar();
+
+/**
+Crea un puntero a un struct tipoAuto y le asigna los valores respectivos.
+
+Recibe un puntero tipo char.
+
+Devuelve un puntero del dato creado.
+*/
 void *crearAutoCSV(char *);
+
+/**
+Crea un puntero a un struct tipoUsuario y le asigna los valores respectivos.
+
+Recibe un puntero tipo char.
+
+Devuelve un puntero del dato creado.
+*/
 void *crearUsuarioCSV(char *);
+
+/**
+
+*/
 void goy(int);
+
 void comprobarMapa(Mapx *); //************************************************
+
+/**
+
+Pregunta al usuario de que forma quiere ver los autos, ya sean tipo, gama o precio.
+
+retorna un puntero.
+*/
 void *mostrarAutos(Mapx *, Mapx *);
+
+/**
+Segun el tipo indicado por parte del usuario se busca y muestran los autos.
+
+retorna un puntero.
+*/
 void *mostrarTipo(Mapx *);
+
+/**
+Segun la gama indicada por parte del usuario se busca y muestran los autos.
+
+retorna un puntero.
+*/
 void *mostrarGama(Mapx *);
+
+/**
+
+*/
 void *menuRecorrer(Mapx *, bool *);
+
+/**
+
+*/
 void imprimirAuto(tipoAuto *);
+
+/**
+Muestra por pantalla el Menu destinado a la Gama de los autos.
+
+Devuelve un int el cual indica la opcion escogida.
+*/
 int menuGama();
+
+/**
+Muestra por pantalla el Menu destinado al tipo de autos.
+
+Devuelve un int el cual indica la opcion escogida.
+*/
 int menuTipo();
+
+/**
+Muestra por pantalla el Menu principal del programa.
+
+Devuelve un int el cual indica la opcion escogida.
+*/
+extern int menu1(void);
+
+/**
+Muestra por pantalla el inicio de la aplicacion.
+*/
+void portada(void);
+
+/**
+Muestra por pantalla las opciones para mostrar los autos del programa.
+
+Devuelve un int el cual indica la opcion escogida.
+*/
+int menuAutos(void);
 
 #endif // FUNCIONES_H_INCLUDED
--- MapaEnlazado.h
+++ MapaEnlazado.h
@@ -5,11 +5,59 @@ typedef struct nodeMapx nodeMapx;
 typedef struct Mapx Mapx;
 typedef struct tipoAuto tipoAuto;
 
+/**
+Crea un nuevo puntero de tipo Mapx.
+
+recibe una funcion equal.
+
+Retorna un puntero al mapa creado.
+*/
 Mapx *createMapx();
+
+/**
+Inserta un elemento no existente en el mapa.
+
+recibe el mapa a ser insertado, la clave y el dato respectivamente.
+*/
 void insertMapx(Mapx *, void *, void *);
+
+/**
+Busca el primer elemento dentro del mapa segun la calve.
+
+recibe el mapa y la clave buscada.
+
+retorna un puntero con la direccion del elemento buscado.
+*/
 void *searchMapx(Mapx *, const void *);
+
+/**
+Entrega un puntero a la posicion actual en que se encuentra el mapa.
+
+recibe el mapa a trabajar.
+
+retorna un puntero a la posicion.
+*/
 void *mapxCurrent(Mapx *);
+
+/**
+Entrega un puntero a la posicion siguiente en que se encuentra el mapa,
+en caso de llegar al final se dirige al inicio.
+
+recibe el mapa a trabajar.
+
+retorna un puntero a la posicion siguiente.
+
+*/
 void *mapxNextList(Mapx *);
+
+/**
+Entrega un puntero a la posicion previa en que se encuentra el mapa,
+en caso de llegar al inicio se devuelve la misma posicion.
+
+recibe el mapa a trabajar.
+
+retorna un puntero a la posicion siguiente.
+*/
 void *mapxPrevList(Mapx *);
 
 #endif // MAPA_ENLAZADO_H_INCLUDED
